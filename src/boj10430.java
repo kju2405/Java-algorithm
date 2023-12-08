c09e31f8278d39ccffe58238142c433cadc894af
Jaeung Kim <kju9730@gmail.com>
2023-12-08 17:16:45 +0900

    boj 10430 [#2]

---
+++ src/boj10430.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class boj10430 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(bufferedReader.readLine(), " ");
+
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+        int C = Integer.parseInt(st.nextToken());
+
+        BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        bufferedWriter.write((A + B) % C + "\n");
+        bufferedWriter.write((((A % C) + (B % C)) % C) + "\n");
+        bufferedWriter.write((A * B) % C + "\n");
+        bufferedWriter.write((((A % C) * (B % C)) % C) + "\n");
+
+        bufferedWriter.flush();
+        bufferedWriter.close();
+    }
+}
