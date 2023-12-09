2dd1a8d46ec059f24a89b5cad328cfc748ec415e
Jaeung Kim <kju9730@gmail.com>
2023-12-09 12:24:54 +0900

    boj 4375

---
+++ src/boj4375.java
@@ -0,0 +1,26 @@
+import java.io.*;
+
+public class boj4375 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        String input;
+
+        while ((input = bufferedReader.readLine()) != null) {
+            int n = Integer.parseInt(input);
+
+            int baseNumber = 1;
+            int cnt = 1;
+
+            while (baseNumber % n != 0) {
+                baseNumber = (baseNumber * 10 + 1) % n;
+                cnt++;
+            }
+
+            bufferedWriter.write(cnt + "\n");
+        }
+        bufferedWriter.flush();
+        bufferedWriter.close();
+    }
+}
