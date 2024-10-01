b24652f149356a53db09f93ea6ad92df1bf28a3e
Jaeung Kim <kju9730@gmail.com>
2024-10-02 00:27:14 +0900

    boj 1260 by dfs&bfs

--- src/Main.java
+++ src/Main.java
@@ -2,58 +2,67 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    private static List<Integer>[] board;
-    private static int[] visited;
-    private static int cnt = 1;
+
+    private static boolean[][] board;
+    private static boolean[] visitedDfs;
+    private static boolean[] visitedBfs;
+    private static int N, M, V;
+
+    private static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        V = Integer.parseInt(st.nextToken());
 
-        int N = Integer.parseInt(st.nextToken());
-        int M = Integer.parseInt(st.nextToken());
-        int R = Integer.parseInt(st.nextToken());
+        board = new boolean[N + 1][N + 1];
+        visitedBfs = new boolean[N + 1];
+        visitedDfs = new boolean[N + 1];
 
-        board = new ArrayList[N + 1];
-        visited = new int[N + 1];
-
-        for (int i = 1; i <= N; i++) {
-            board[i] = new ArrayList<>();
-        }
-
-        for (int i = 0; i < M; i++) {
+        while (M-- > 0) {
             st = new StringTokenizer(br.readLine());
             int x = Integer.parseInt(st.nextToken());
             int y = Integer.parseInt(st.nextToken());
 
-            board[x].add(y);
-            board[y].add(x);
+            board[x][y] = true;
+            board[y][x] = true;
         }
 
-        for (int i = 1; i <= N; i++) {
-            Collections.sort(board[i], Collections.reverseOrder());
-        }
-
-        dfs(R);
-
-        for (int i = 1; i <= N; i++) {
-            bw.write(String.valueOf(visited[i]));
-            bw.newLine();
-        }
+        dfs(V);
+        bw.newLine();
+        bfs(V);
 
         br.close();
         bw.close();
     }
 
-    private static void dfs(int idx) {
-        visited[idx] = cnt++;
+    private static void dfs(int idx) throws IOException {
+        bw.write(idx + " ");
+        visitedDfs[idx] = true;
 
-        for (int i = 0; i < board[idx].size(); i++) {
-            int num = board[idx].get(i);
-            if (visited[num] == 0) {
-                dfs(num);
+        for (int i = 1; i <= N; i++) {
+            if (board[idx][i] && !visitedDfs[i]) {
+                dfs(i);
+            }
+        }
+    }
+
+    private static void bfs(int idx) throws IOException {
+        ArrayDeque<Integer> queue = new ArrayDeque<>();
+        queue.addLast(idx);
+        visitedBfs[idx] = true;
+
+        while (!queue.isEmpty()) {
+            int cur = queue.pollFirst();
+            bw.write(cur + " ");
+            for (int i = 1; i <= N; i++) {
+                if (board[cur][i] && !visitedBfs[i]) {
+                    queue.addLast(i);
+                    visitedBfs[i] = true;
+                }
             }
         }
     }
