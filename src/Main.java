9a6af86a41d8617b794feb333c80db7033e03616
Jaeung Kim <kju9730@gmail.com>
2024-10-04 21:49:16 +0900

    boj 1012 by dfs

--- src/Main.java
+++ src/Main.java
@@ -1,38 +1,45 @@
 import java.io.*;
-import java.util.*;
+import java.util.StringTokenizer;
 
 public class Main {
-
-    private static List<Integer>[] board;
-    private static int[] visited;
-    private static int n;
+    private static boolean[][] board;
+    private static boolean[][] visited;
+    private static int[] dx = {0, 1, 0, -1};
+    private static int[] dy = {-1, 0, 1, 0};
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        n = Integer.parseInt(br.readLine());
+        int testCase = Integer.parseInt(br.readLine());
 
-        board = new ArrayList[n + 1];
-        visited = new int[n + 1];
-
-        for (int i = 1; i <= n; i++) {
-            board[i] = new ArrayList<>();
-        }
-
-        for (int i = 0; i < n - 1; i++) {
+        while (testCase-- > 0) {
             StringTokenizer st = new StringTokenizer(br.readLine());
-            int x = Integer.parseInt(st.nextToken());
-            int y = Integer.parseInt(st.nextToken());
+            int rowSizse = Integer.parseInt(st.nextToken());
+            int colSize = Integer.parseInt(st.nextToken());
+            int cnt = Integer.parseInt(st.nextToken());
 
-            board[x].add(y);
-            board[y].add(x);
-        }
+            board = new boolean[rowSizse][colSize];
+            visited = new boolean[rowSizse][colSize];
 
-        dfs(1, 1);
+            while (cnt-- > 0) {
+                st = new StringTokenizer(br.readLine());
+                int row = Integer.parseInt(st.nextToken());
+                int col = Integer.parseInt(st.nextToken());
 
-        for (int i = 2; i <= n; i++) {
-            bw.write(String.valueOf(visited[i]));
+                board[row][col] = true;
+            }
+            int answer = 0;
+            for (int i = 0; i < rowSizse; i++) {
+                for (int j = 0; j < colSize; j++) {
+                    if (board[i][j] && !visited[i][j]) {
+                        dfs(i, j);
+                        answer++;
+                    }
+                }
+            }
+
+            bw.write(String.valueOf(answer));
             bw.newLine();
         }
 
@@ -40,15 +47,18 @@ public class Main {
         bw.close();
     }
 
-    private static void dfs(int idx, int parent) {
-        if (visited[idx] != 0) {
-            return;
-        }
-        visited[idx] = parent;
+    private static void dfs(int row, int col) {
+        visited[row][col] = true;
 
-        for (int i = 0; i < board[idx].size(); i++) {
-            int num = board[idx].get(i);
-            dfs(num, idx);
+        for (int i = 0; i < 4; i++) {
+            int nx = row + dx[i];
+            int ny = col + dy[i];
+            if (nx < 0 || nx >= board.length || ny < 0 || ny >= board[0].length) {
+                continue;
+            }
+            if (board[nx][ny] && !visited[nx][ny]) {
+                dfs(nx, ny);
+            }
         }
     }
 }
